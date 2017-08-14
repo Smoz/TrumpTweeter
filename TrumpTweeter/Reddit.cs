7161260d24175bd492a15e3f71e11670b906e576
Smoz <Steve.Moseley@g.austincc.edu>
2017-08-14 11:47:07 -0500

    Added a check for image sites

--- TrumpTweeter/Reddit.cs
+++ TrumpTweeter/Reddit.cs
@@ -14,7 +14,50 @@ namespace TrumpTweeter
         {
             string url = Globals.url;
             HtmlDocument doc = new HtmlWeb().Load(url);
-            ScrapeImage(doc);
+            CheckForImages(doc);
+            //ScrapeImage(doc);
+        }
+
+        // We'll use this method to check if there are
+        // any images/memes to upload by checking the
+        // domain of the site on the listing
+        // If a site is detected then we get the url
+        // of the linked image and proceed to call
+        // our ScrapeImage method
+
+        static void CheckForImages(HtmlDocument doc)
+        {
+            string s1 = "i.redd.it";
+            string s2 = "i.imgur.com";
+
+            var spans = doc.DocumentNode.SelectNodes("//span[@class='domain']").ToArray();
+            // Check if images are present
+
+            foreach (var span in spans)
+            {
+                string getImageAndMemeSpanText = span.InnerText;
+
+                bool iReddit = getImageAndMemeSpanText.Contains(s1);
+                bool imgur = getImageAndMemeSpanText.Contains(s2);
+
+                if (iReddit || imgur == true)
+                {
+                    Console.WriteLine("Target image site detected!");
+
+                    // If the an image site is detected then we would call
+                    // ScrapeImage to scrape that image.
+                    // So we add the domain to a local variable and pass
+                    // that variable to our scraper
+
+                    // ScrapeImage(doc);
+                }
+                else if (iReddit && imgur == false)
+                {
+                    Console.WriteLine("Sorry, no new images/memes right now...boop boop..boooop. :((");
+                }
+
+            }
+
         }
 
         // We'll use this method to scrape images/memes
